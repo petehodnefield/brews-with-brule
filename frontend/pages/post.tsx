1a3008491fd80423c586e90436d45c179263afc5
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-01-24 08:59:49 -0600

    post form mobile added

--- frontend/pages/post.tsx
+++ frontend/pages/post.tsx
@@ -12,9 +12,9 @@ const post = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <div className='flex flex-col items-center'>
-                    <h2 className='font-semibold text-1.5'>Create a post</h2>
-                    <form className='w-full px-6'>
+                <div className='flex flex-col items-center justify-center pt-12'>
+                    <h2 className='font-semibold text-1.5 mb-8'>Create a post</h2>
+                    <form className='w-full px-6 flex flex-col items-center justify-center'>
                         <div className='flex items-left flex-col mb-8 w-full lg:w-80'>
                             <div className='flex justify-between items-center w-full mb-1'>
                                 <label className='font-semibold text-0.875 '>Title</label>
@@ -22,6 +22,52 @@ const post = () => {
                             </div>
                             <input className='rounded-lg h-12 p-4 font-semibold text-0.875 border-solid border-light border-2' name='username' type='text'></input>
                         </div>
+                        <div className='flex items-left flex-col mb-8 w-full lg:w-80'>
+                            <div className='flex justify-between items-center w-full mb-1'>
+                                <label className='font-semibold text-0.875 '>Description</label>
+                                <p className='text-0.875 text-medium italic'>160/160 remaining</p>
+                            </div>
+                            <textarea className='rounded-lg h-24 p-4 font-semibold text-0.875 border-solid border-light border-2' name='username' ></textarea>
+                        </div>
+                        <div className='flex items-left flex-col mb-8 w-full lg:w-80'>
+
+                            <label className='font-semibold text-0.875  mb-1 '>Location</label>
+                            <input className='rounded-lg h-12 p-4 font-semibold text-0.875 border-solid border-light border-2' name='location' type='text' placeholder="City, State"></input>
+                        </div>
+                        <div className='flex items-left flex-col mb-4 w-full lg:w-80'>
+
+                            <label className='font-semibold text-0.875  mb-1 '>Photo</label>
+                            <input type="file" className='h-12'>
+                            </input>
+                        </div>
+                        <div className='flex items-center justify-around  mb-4 w-full lg:w-80'>
+                            <p>Tag a friend(s)</p>
+                            <div className=' w-16 h-10 border-solid border-light border-2 rounded-full flex items-center justify-end'>
+                                <div className='bg-primary rounded-full h-8 w-8 mr-1'></div>
+                            </div>
+                        </div>
+
+                        {/* FRIENDS LIST */}
+                        <section className='w-full border-light border-solid border-t-2 mb-8'>
+                            <div className='flex justify-between items-center w-full border-light border-solid border-b-2 py-4	'>
+                                <img className='h-16 rounded-lg' src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' />
+                                <h4 className='w-60 ml-4'>Max Friedrich</h4>
+                                <div className='h-6 w-6  border-light border-2'></div>
+                            </div>
+                            <div className='flex justify-between items-center w-full border-light border-solid border-b-2 py-4	'>
+                                <img className='h-16 rounded-lg' src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' />
+                                <h4 className='w-60 ml-4'>Logan Swanson</h4>
+                                <div className='h-6 w-6  border-light border-2'></div>
+                            </div>
+                            <div className='flex justify-between items-center w-full border-light border-solid border-b-2 py-4	'>
+                                <img className='h-16 rounded-lg' src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' />
+                                <h4 className='w-60 ml-4'>Tanner Greeley</h4>
+                                <div className='h-6 w-6  border-light border-2'></div>
+                            </div>
+                        </section>
+
+                        {/* SUBMIT BTN */}
+                        <button type='submit' className='bg-primary w-44 h-12 text-white mb-12'>Submit</button>
                     </form>
                 </div>
             </main>
