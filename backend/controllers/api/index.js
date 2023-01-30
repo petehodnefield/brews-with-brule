093c86aa88481af4327c0880be2936989f85e288
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-01-30 06:29:03 -0600

    added Post model and CRUD routes

--- backend/controllers/api/index.js
+++ backend/controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes')
-
 router.use('/users', userRoutes)
+
+const postRoutes = require('./post-routes')
+router.use('/posts', postRoutes)
 
 module.exports = router
