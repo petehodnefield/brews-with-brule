b50e8257617e0e6e92ef4acce335e3e1124ec11f
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-02-01 15:11:42 -0600

    added hard coded api routes

--- frontend/lib/db.js
+++ frontend/lib/db.js
@@ -7,10 +7,18 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "mysql",
     port: 3306,
   }
+  
 );
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} 
+catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
 
 module.exports = sequelize;
