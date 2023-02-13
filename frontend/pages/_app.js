a1c3b359d96e391831d4105a35724b858b893823
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-02-13 07:59:35 -0600

    added favicon

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,37 +1,18 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import {setContext} from '@apollo/client/link/context'
 import { useApollo } from '../lib/apollo';
+
 
 
 
 export default function App({ Component, pageProps }) {
 
-  // const httpLink = createHttpLink({
-  //   uri: 'https://bwb-api.onrender.com/graphql',
-  // })
-
-
-  // const authLink = setContext((_, {headers}) => {
-  //   const token = localStorage.getItem('id_token')
-  //   return {
-  //     headers: {
-  //       ...headers,
-  //       authorization: token ? `Bearer ${token}` : '',
-  //     },
-  //   }
-  // })
-
-  // const client = new ApolloClient({
-  //   ssrMode: typeof window === 'undefined',
-  //   link:  authLink.concat(httpLink),
-  //   cache: new InMemoryCache(),
-  // });
   const apolloClient = useApollo(pageProps.initialApolloState)
   
   return (
     <ApolloProvider client={apolloClient}>
+       
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,14 +3,13 @@ import FooterMobile from '../components/Footer/FooterMobile'
 import Header from '../components/Header/Header'
 import { Html, Head, Main, NextScript } from 'next/document'
 
-
 export default function Document() {
   
 
   return (
 
       <Html lang="en">
-          <Head />
+        <Head/>
           <body >
               <Main />
               <NextScript />
